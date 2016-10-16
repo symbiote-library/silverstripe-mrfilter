8c630911f492cc748cb9b17a820e325ec5edc315
Jake Bentvelzen <doogie1012@gmail.com>
2016-10-16 20:46:07 +1100

    feat(ListFilterAdmin): Add icon to ModelAdmin

--- code/admin/ListFilterAdmin.php
+++ code/admin/ListFilterAdmin.php
@@ -4,4 +4,6 @@ class ListFilterAdmin extends ModelAdmin {
 	private static $url_segment = 'list-filter-sets';
 	private static $menu_title = 'List Filter Sets';
 	private static $managed_models = array('ListFilterSet');
+	private static $menu_icon = 'mrfilter/images/funnel_icon.png';
+	private static $icon = "event_calendar/images/calendar";
 }
